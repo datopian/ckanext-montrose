3f47be6798e5e4e4aeb3c04b3c2b29bfbe87ffb1
Damjan Velickovski <damjan91@gmail.com>
2016-05-25 15:24:57 +0200

    Add search and filters

--- ckanext/montrose/plugin.py
+++ ckanext/montrose/plugin.py
@@ -1,13 +1,50 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-import ckan.lib.base as base
-from datetime import date, datetime
+import ckan.lib.plugins as lib_plugins
+from ckan.controllers.package import (PackageController,
+                                      url_with_params,
+                                      _encode_params)
 
 from logging import getLogger
 
+import logging
+from urllib import urlencode
+from datetime import datetime
+
+from pylons import config
+from paste.deploy.converters import asbool
+
+import ckan.logic as logic
+import ckan.lib.base as base
+import ckan.lib.maintain as maintain
+import ckan.lib.helpers as h
+import ckan.model as model
+import ckan.plugins as p
+import ckan.lib.render
+
+from ckan.common import OrderedDict, _, json, request, c, g, response
+
+log = logging.getLogger(__name__)
+
+render = base.render
+abort = base.abort
+redirect = base.redirect
+
+NotAuthorized = logic.NotAuthorized
+ValidationError = logic.ValidationError
+check_access = logic.check_access
+get_action = logic.get_action
+tuplize_dict = logic.tuplize_dict
+clean_dict = logic.clean_dict
+parse_params = logic.parse_params
+flatten_to_string_key = logic.flatten_to_string_key
+
+lookup_package_plugin = ckan.lib.plugins.lookup_package_plugin
+
 logger = getLogger(__name__)
 
-class MontrosePlugin(plugins.SingletonPlugin):
+
+class MontrosePlugin(plugins.SingletonPlugin, lib_plugins.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer, inherit=True)
     plugins.implements(plugins.IResourceView, inherit=True)
     plugins.implements(plugins.IRoutes, inherit=True)
@@ -21,24 +58,30 @@ class MontrosePlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'montrose')
 
-    def after_map(self, map_):
+    def before_map(self, map_):
         controller = 'ckanext.montrose.plugin:DashboardsController'
 
         map_.connect('/kenya', controller=controller, action='kenya')
+        # with SubMapper(map_, controller=controller) as m:
+        #    m.connect('search', '/kenya/search', action='search',
+        #             highlight_actions='index search')
 
         return map_
 
     def get_helpers(self):
         return {
             'montrose_get_newly_released_data':
-                montrose_get_newly_released_data
+                montrose_get_newly_released_data,
+            'montrose_convert_time_format':
+                montrose_convert_time_format,
+            'replace_or_add_url_param':
+                replace_or_add_url_param
         }
 
 
-
-class DashboardsController(base.BaseController):
+class DashboardsController(PackageController):
     def kenya(self):
-        '''map_resource_view_id = '882d4d34-895b-4ff2-8f41-2ba916665734'
+        map_resource_view_id = '72f7154b-1659-4b87-b1fe-f7dd11b2ef03'
         data_dict = {
             'id': map_resource_view_id
         }
@@ -54,14 +97,12 @@ class DashboardsController(base.BaseController):
         }
         map_package = toolkit.get_action('package_show')({}, data_dict)
 
-        extra = {'map_package': map_package,
+        map_extra = {'map_package': map_package,
                  'map_resource': map_resource,
                  'map_resource_view': map_resource_view}
 
-        return plugins.toolkit.render('dashboards/kenya.html', extra_vars=extra)'''
-
         ids = ['f111fa89-44f3-47d8-a584-eecbea046cd8',
-               'db91e252-43a6-4520-958c-f226a0acedad',
+               '51e8b616-ed4c-49e5-be7c-36732ab2741b',
                'f0186ced-847c-4367-88f0-d5641bf0117e',
                '0a844f3b-a0a4-4ba1-af07-302607105b3e',
                '015abce5-7102-4457-b912-861491bef523',
@@ -71,7 +112,184 @@ class DashboardsController(base.BaseController):
         for id in ids:
             charts.append(get_resourceview_resource_package(id))
 
-        return plugins.toolkit.render('dashboards/kenya.html', extra_vars={'charts': charts})
+        from ckan.lib.search import SearchError
+
+        package_type = self._guess_package_type()
+
+        try:
+            context = {'model': model, 'user': c.user or c.author,
+                       'auth_user_obj': c.userobj}
+            check_access('site_read', context)
+        except NotAuthorized:
+            abort(401, _('Not authorized to see this page'))
+
+        # unicode format (decoded from utf8)
+        q = c.q = request.params.get('q', u'')
+        c.query_error = False
+        page = self._get_page_number(request.params)
+
+        limit = g.datasets_per_page
+
+        # most search operations should reset the page counter:
+        params_nopage = [(k, v) for k, v in request.params.items()
+                         if k != 'page']
+
+        def drill_down_url(alternative_url=None, **by):
+            return h.add_url_param(alternative_url=alternative_url,
+                                   controller='package', action='search',
+                                   new_params=by)
+
+        c.drill_down_url = drill_down_url
+
+        def remove_field(key, value=None, replace=None):
+            return h.remove_url_param(key, value=value, replace=replace,
+                                      controller='package', action='search')
+
+        c.remove_field = remove_field
+
+        sort_by = request.params.get('sort', None)
+        params_nosort = [(k, v) for k, v in params_nopage if k != 'sort']
+
+        def _sort_by(fields):
+            """
+            Sort by the given list of fields.
+
+            Each entry in the list is a 2-tuple: (fieldname, sort_order)
+
+            eg - [('metadata_modified', 'desc'), ('name', 'asc')]
+
+            If fields is empty, then the default ordering is used.
+            """
+            params = params_nosort[:]
+
+            if fields:
+                sort_string = ', '.join('%s %s' % f for f in fields)
+                params.append(('sort', sort_string))
+            return search_url(params, package_type)
+
+        c.sort_by = _sort_by
+        if not sort_by:
+            c.sort_by_fields = []
+        else:
+            c.sort_by_fields = [field.split()[0]
+                                for field in sort_by.split(',')]
+
+        def pager_url(q=None, page=None):
+            params = list(params_nopage)
+            params.append(('page', page))
+            return search_url(params, package_type)
+
+        c.search_url_params = urlencode(_encode_params(params_nopage))
+
+        try:
+            c.fields = []
+            # c.fields_grouped will contain a dict of params containing
+            # a list of values eg {'tags':['tag1', 'tag2']}
+            c.fields_grouped = {}
+            search_extras = {}
+            fq = ''
+            for (param, value) in request.params.items():
+                if param not in ['q', 'page', 'sort'] \
+                        and len(value) and not param.startswith('_'):
+                    if not param.startswith('ext_'):
+                        c.fields.append((param, value))
+                        fq += ' %s:"%s"' % (param, value)
+                        if param not in c.fields_grouped:
+                            c.fields_grouped[param] = [value]
+                        else:
+                            c.fields_grouped[param].append(value)
+                    else:
+                        search_extras[param] = value
+
+            context = {'model': model, 'session': model.Session,
+                       'user': c.user or c.author, 'for_view': True,
+                       'auth_user_obj': c.userobj}
+
+            if package_type and package_type != 'dataset':
+                # Only show datasets of this particular type
+                fq += ' +dataset_type:{type}'.format(type=package_type)
+            else:
+                # Unless changed via config options, don't show non standard
+                # dataset types on the default search page
+                if not asbool(
+                        config.get('ckan.search.show_all_types', 'False')):
+                    fq += ' +dataset_type:dataset'
+
+            facets = OrderedDict()
+
+            default_facet_titles = {
+                'organization': _('Organizations'),
+                'groups': _('Groups'),
+                'tags': _('Tags'),
+                'res_format': _('Formats'),
+                'license_id': _('Licenses'),
+            }
+
+            for facet in g.facets:
+                if facet in default_facet_titles:
+                    facets[facet] = default_facet_titles[facet]
+                else:
+                    facets[facet] = facet
+
+            # Facet titles
+            for plugin in p.PluginImplementations(p.IFacets):
+                facets = plugin.dataset_facets(facets, package_type)
+
+            c.facet_titles = facets
+
+            data_dict = {
+                'q': q,
+                'fq': fq.strip(),
+                'facet.field': facets.keys(),
+                'rows': limit,
+                'start': (page - 1) * limit,
+                'sort': sort_by,
+                'extras': search_extras
+            }
+
+            query = get_action('package_search')(context, data_dict)
+            c.sort_by_selected = query['sort']
+
+            c.page = h.Page(
+                collection=query['results'],
+                page=page,
+                url=pager_url,
+                item_count=query['count'],
+                items_per_page=limit
+            )
+            c.facets = query['facets']
+            c.search_facets = query['search_facets']
+            c.page.items = query['results']
+        except SearchError, se:
+            log.error('Dataset search error: %r', se.args)
+            c.query_error = True
+            c.facets = {}
+            c.search_facets = {}
+            c.page = h.Page(collection=[])
+        c.search_facets_limits = {}
+        for facet in c.search_facets.keys():
+            try:
+                limit = int(request.params.get('_%s_limit' % facet,
+                                               g.facets_default_number))
+            except ValueError:
+                abort(400, _('Parameter "{parameter_name}" is not '
+                             'an integer').format(
+                    parameter_name='_%s_limit' % facet))
+            c.search_facets_limits[facet] = limit
+
+        maintain.deprecate_context_item(
+            'facets',
+            'Use `c.search_facets` instead.')
+
+        self._setup_template_variables(context, {},
+                                       package_type=package_type)
+
+        return plugins.toolkit.render('dashboards/kenya.html', extra_vars={'charts': charts,
+                                                                           'dataset_type': package_type,
+                                                                           'map': map_extra})
+
+    def _guess_package_type(self, expecting_name=False):
+        return 'dataset'
 
 
 def get_resourceview_resource_package(resource_view_id):
@@ -112,3 +330,29 @@ def montrose_get_newly_released_data(limit=5):
             package['human_metadata_modified'] = modified.strftime("%d %B %Y")
             pkgs.append(package)
         return pkgs
+
+
+def montrose_convert_time_format(package):
+    modified = datetime.strptime(package['metadata_modified'].split('T')[0], '%Y-%m-%d')
+    return modified.strftime("%d %B %Y")
+
+
+def replace_or_add_url_param(name, value):
+    params = request.params.items()
+    #params = set(params)
+
+    for k, v in params:
+        if k != name:
+            continue
+        params.remove((k, v))
+
+    params.append((name, value))
+
+    controller = c.controller
+    action = c.action
+
+    url = h.url_for(controller=controller, action=action)
+
+    params = [(k, v.encode('utf-8') if isinstance(v, basestring) else str(v))
+                  for k, v in params]
+    return url + u'?' + urlencode(params)
