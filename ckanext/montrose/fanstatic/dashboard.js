e6812882cee7211d94da17a8efc3d42fd14d82ba
Petar Efnushev <petar.efnushev@gmail.com>
2016-06-30 12:35:58 +0200

    Add show/hide functionallity for newly released data

--- ckanext/montrose/fanstatic/dashboard.js
+++ ckanext/montrose/fanstatic/dashboard.js
@@ -6,6 +6,24 @@ $(function () {
     }
     return false;
   });
+
+ $(document).ready(function(){
+ 	  $('#newly-released-data-btn').on('click', function(){
+ 	  	console.log('detected click');
+ 	  	if($('#newly-released-data').hasClass('hidden')){
+ 	  		$('#newly-released-data-btn').html('');
+ 	  		$('#newly-released-data').removeClass('hidden');
+ 	  		$('#newly-released-data-btn').html('<i class="fa fa-compress pull-right"></i>');
+
+ 	  	}
+ 	  	else{
+ 	  		$('#newly-released-data-btn').html('');
+ 	  		$('#newly-released-data').addClass('hidden');
+ 	  		$('#newly-released-data-btn').html('<i class="fa fa-expand pull-right"></i>');
+ 	  	}
+ 	  });
+  });
+
 });
 
 function toggleResources(resourceId) {
